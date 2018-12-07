fix a bug in the fitrah calculation.

evolution.py
@@ -15,7 +15,7 @@ class Evolution:
                       np.mean([dna1.learning_frequency(), dna2.learning_frequency()]),
                       np.mean([dna1.life_expectancy(), dna2.life_expectancy()]),
                       np.mean([dna1.gamma(), dna2.gamma()]),
-                      np.mean([dna1.fitrah(), dna2.fitrah()]))
+                      np.mean([dna1.fitrah(), dna2.fitrah()], axis=0))
         return Evolution.mutate_dna(new_dna)
 
     @staticmethod
@@ -26,7 +26,10 @@ class Evolution:
         learning_frequency = max(dna.learning_frequency() + randint(-1, 1), 1)
         life_expectancy = max(0, dna.life_expectancy() + randint(-10, 10))
         gamma = max(0.1, min(1, np.random.normal(loc=dna.gamma(), scale=0.001)))
-        fitrah = utils.softmax(dna.fitrah() + np.random.normal(loc=0, scale=0.1, size=dna.fitrah().size), dna.fitrah().size)
+        fitrah = utils.softmax(
+            + np.random.normal(loc=0, scale=0.1, size=dna.fitrah().size), dna.fitrah().size)
+        if fitrah.size==1:
+            print('found')
         return DNA(memory_size, learning_rate, hidden_layer_size, learning_frequency, life_expectancy, gamma, fitrah)
 
 
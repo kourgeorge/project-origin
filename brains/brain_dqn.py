Removed the softmax from the network of the Tensorflow DQN. Fixed the load model fof torch dqn. The run step of universe return the number of living creatures at the end of the step.

brains/brain_dqn.py
@@ -56,7 +56,7 @@ class BrainDQN(AbstractBrain):
         return np.sum([np.prod(v.get_shape().as_list()) for v in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, self._scope)])
 
     def think(self, obs):
-        q_value = BrainDQN.sess.run(self.QValue, feed_dict={self.state_in: [obs]})[0]
+        q_value = utils.softmax(BrainDQN.sess.run(self.QValue, feed_dict={self.state_in: [obs]})[0])
         return q_value
 
     def train(self, experience):
@@ -100,7 +100,7 @@ class BrainDQN(AbstractBrain):
             state = slim.max_pool2d(state, [2, 2])
             state = slim.flatten(state)
             state = slim.stack(state, slim.fully_connected, [self._h_size], activation_fn=tf.nn.relu)
-            action_output = slim.fully_connected(state, self.num_actions(), activation_fn=tf.nn.softmax,
+            action_output = slim.fully_connected(state, self.num_actions(), activation_fn=None,
                                                  weights_regularizer=slim.l2_regularizer(
                                                      self._regularization_param))
 

universe.py
@@ -18,8 +18,6 @@ class Universe:
         self.statistics = statistics
         for race in races:
             num_fathers = ConfigPhysics.NUM_FATHERS
-            if race.race_name() == 'Zombie':
-                num_fathers = 2*ConfigPhysics.NUM_FATHERS
             fathers_locations_i = np.random.choice(ConfigPhysics.SPACE_SIZE, num_fathers)
             fathers_locations_j = np.random.choice(ConfigPhysics.SPACE_SIZE, num_fathers)
             for n in range(num_fathers):
@@ -54,12 +52,13 @@ class Universe:
     # Time Management
     def pass_time(self):
         self._time += 1
-        if self._time < ConfigPhysics.ETERNITY and self.num_creatures() > 0:
-            self.give_food(round(self.num_creatures() * ConfigPhysics.FOOD_CREATURE_RATIO))
+        num_creatures = self.num_creatures()
+        if self._time < ConfigPhysics.ETERNITY and num_creatures > 0:
+            self.give_food(round(num_creatures * ConfigPhysics.FOOD_CREATURE_RATIO))
             for creature in self.get_all_creatures():
                 if creature.alive():
                     self.execute_action(creature)
-            return self._time
+            return self.num_creatures()
         else:
             return None
 
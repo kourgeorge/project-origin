1. Action distribution data collection in statistics. 2. Fixing the softmax implenentation.

statistics.py
@@ -12,7 +12,7 @@ from config import ConfigBrain, ConfigBiology
 class Stats:
 
     def __init__(self):
-        self.action_dist = np.zeros(Actions.num_actions())  # [Left Right Eat Mate Fight]
+        self.action_dist = []  # [Left Right Eat Mate Fight]
         self.death_cause = [0, 0, 0, 0]  # [Fatigue Fight Elderly Fall]
         self.step_stats_df = pd.DataFrame()
         self.epoch_stats_df = pd.DataFrame()
@@ -27,7 +27,7 @@ class Stats:
             ('Time', universe.get_time()),
             ('Population', universe.num_creatures()),
             ('IDs', universe.get_creatures_counter()),
-            ('Age', np.round(utils.emptynanmean([creature.age() for creature in universe.get_all_creatures()]), 2)),
+            ('MeanAge', np.round(utils.emptynanmean([creature.age() for creature in universe.get_all_creatures()]), 2)),
             ('MaxAge',
              np.round(utils.emptynanmean([creature.life_expectancy() for creature in universe.get_all_creatures()]), 2)),
             ('BrainParam',
@@ -46,7 +46,7 @@ class Stats:
              np.round(utils.emptynanmean([creature.vision_range() for creature in universe.get_all_creatures()]), 2)),
             ('AIQ', AIQ.get_population_aiq(universe)),
             ('RacesDist', universe.races_dist()),
-            ('ActionDist', self.action_dist),
+            ('ActionDist', self.actions_dist_hist()),
             ('DeathCause', self.death_cause),
             ('CreaturesDist', universe.get_creatures_distribution()),
             ('FoodDist', universe.get_food_distribution()),
@@ -69,5 +69,9 @@ class Stats:
         ])
 
     def initialize_inter_step_stats(self):
-        self.action_dist = np.zeros_like(self.action_dist)
+        self.action_dist = []
         self.death_cause = np.zeros_like(self.death_cause)
+
+    def actions_dist_hist(self):
+        actions = [creature_action_log[2] for creature_action_log in self.action_dist]
+        return np.histogram (actions, bins=range(0,Actions.num_actions()))[0]

utils.py
@@ -29,10 +29,11 @@ def epsilon_greedy(eps, dist):
 
 
 def dist_selection(dist):
-    select_prob = np.random.choice(dist, p=dist)
-    selection = np.argmax(dist == select_prob)
+    if sum(dist) != 1:
+        dist[0] = dist[0] + (1 - sum(dist))
+    action = np.argmax(np.random.multinomial(1, dist))
 
-    return selection
+    return action
 
 
 # Arg is an int and size is the len of the returning vector
@@ -58,10 +59,10 @@ def softmax(x, temprature=1):
     Columns are predictions (samples).
     """
     # x = normalize(np.reshape(x, (1, -1)), norm='l2')[0]
-    ex_x = np.exp(temprature * np.subtract(x, max(x)))
+    ex_x = np.exp(temprature * np.subtract(x, np.max(x)))
     if np.isinf(np.sum(ex_x)):
         raise Exception('Inf in softmax')
-    return ex_x / ex_x.sum(0)
+    return ex_x / np.sum(ex_x)
 
 
 def roll_fight(energy1, energy2):